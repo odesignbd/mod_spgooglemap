720b5b9a5fe47574b1d8e85a58a25acf025073f2
SymphonyTheme <odesignbd@gmail.com>
2021-10-21 15:52:41 +0600

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -22,7 +22,7 @@ $doc = Factory::getDocument();
 $modPath = Uri::root(true).'/modules/'.$module->module;
 $doc->addStylesheet( $modPath.'/assets/css/odesign.css' );
 $doc->addScript( $modPath.'/assets/js/odesign.js' );
-echo 'Saikot';
+echo 'Saikot2';
 ?>
 
 <!--====== Main Map Area =====-->
